2229ab4f6ac818fede6351fcbbbee1432685ab21
Ronc122 <cajanr02@gmail.com>
2018-12-24 20:37:51 +0800

    Deleted to organize files

--- error1.php
+++
@@ -1,7 +0,0 @@
-<?php if (count($error1) > 0): ?>
-	<div class="error1">
-		<?php foreach ($error1 as $error1): ?>
-			<center><p><?php echo $error1; ?></p></center>
-		<?php endforeach ?>
-	</div>
-<?php endif ?>
